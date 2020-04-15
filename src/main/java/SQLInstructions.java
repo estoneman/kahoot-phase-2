040f61c1d7e06f6d9e7c60ef97e633249e6e70da
estoneman <estoneman@luc.edu>
2020-04-15 01:47:08 -0500

    added methods for creating tables and databases as well as creating a new way to connect to a specific database

--- src/main/java/SQLInstructions.java
+++ src/main/java/SQLInstructions.java
@@ -2,17 +2,20 @@
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Scanner;
 
-class ConnectToDatabase {
+class SQLInstructions {
 
 //    private static final String CONNECTION_ESTABLISHED = "Connection established . . .";
 //    private static final String CONNECTON_FAILED = "Connection failed";
     private static final String JDBC_PACKAGE = "com.mysql.cj.jdbc.Driver";
-    private static final String MySQLPollURL = "jdbc:mysql://192.168.0.21:3306/poll";
+    private static final String MySQLPollURL = "jdbc:mysql://192.168.0.21:3306/";
     private static final String USER = "MySQLAdmin2";
     private static final String PASS = "19Soccer99)";
 
-    static Connection connectToPollDB() {
+    static Connection connectToSQL() {
         Connection connection = null;
 
         try {
@@ -28,6 +31,102 @@ class ConnectToDatabase {
 
 //        System.out.println(CONNECTION_ESTABLISHED);
         return connection;
+    }
+
+    static Connection connectToPollDB(String dBName) {
+        Connection connection = null;
+
+        try {
+            //Registering the Driver
+            Class.forName(JDBC_PACKAGE);
+            //Getting the connection
+            connection = DriverManager.getConnection(MySQLPollURL + dBName, USER, PASS);
+        }
+        catch (Exception e) {
+//            System.out.println(CONNECTON_FAILED);
+            e.printStackTrace();
+        }
+
+//        System.out.println(CONNECTION_ESTABLISHED);
+        return connection;
+    }
+
+    static void createDatabase(String dBName) {
+
+        Statement statement = null;
+        Connection connection = null;
+
+        try {
+            connection = connectToSQL();
+
+            statement = connection.createStatement();
+
+            //instructions for creating databse in SQL
+            String sqlInstructions = "CREATE DATABASE " + dBName + ";";
+
+            //execute creating new database
+            statement.executeUpdate(sqlInstructions);
+
+            System.out.println(dBName + " was created successfully");
+        }
+        catch (SQLException sQLE) {
+            sQLE.printStackTrace();
+        }
+        //handles closing connection and statement variables
+        finally {
+            try {
+                if (statement != null)
+                    statement.close();
+                if (connection != null)
+                    connection.close();
+            }
+            //if statement or connection cannot close cannot close
+            catch (SQLException sQLE) {
+                sQLE.printStackTrace();
+            }
+        }
+
+    }
+
+    static void createTable(String tableName, String dBName) {
+
+        Connection connection = null;
+        Statement statement = null;
+
+        try {
+            connection = connectToPollDB(dBName);
+
+            statement = connection.createStatement();
+
+            //instructions for creating a table in SQL
+            String sqlInstructions = "CREATE TABLE " + dBName + "." + tableName +
+                    "(QuestionNumber INT NOT NULL AUTO_INCREMENT, " +
+                    " Question VARCHAR(100), " +
+                    " Options VARCHAR(150), " +
+                    " PRIMARY KEY (QuestionNumber));";
+
+            //execute creating the table
+            statement.executeUpdate(sqlInstructions);
+
+            System.out.println(tableName + " was created successfully");
+        }
+        //if statement cannot be created
+        catch (SQLException sQLE) {
+            sQLE.printStackTrace();
+        }
+        //for closing statement and connection
+        finally {
+            try {
+                if (statement != null)
+                    statement.close();
+                if (connection != null)
+                    connection.close();
+            }
+            catch (SQLException sQLE) {
+                sQLE.printStackTrace();
+            }
+        }
+
     }
 
 }
